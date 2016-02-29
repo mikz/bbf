f6a06dd1b549eabfb2384252c4c1e53d4a137e40
Michal Cichra <michal@cichra.cz>
2016-03-01 00:04:09 +0100

    make clicking work in material-ui

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, Link, browserHistory, IndexRoute } from 'react-router'
 
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+
 import { App, Home } from './App';
 import { Beers } from './Beers';
 import { Taps, TapVote } from './Taps';
